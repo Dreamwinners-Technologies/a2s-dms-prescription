082cdf1666b10d4a718cd873aef294a85451cd0a
Faisul-Islam <suhid15@gmail.com>
2021-09-20 02:19:34 +0600

    fixed npm related issues

---
+++ src/service/drugs_service.js
@@ -0,0 +1,65 @@
+import { connection } from "./jsstore_con";
+
+export class DrugService {
+
+    constructor() {
+
+        this.tableName = "Drugs";
+    }
+
+    getDrugs() {
+        return connection.select({
+            from: this.tableName,
+        })
+    }
+
+    addDrugs(drug) {
+        return connection.insert({
+            into: this.tableName,
+            values: [drug],
+            return: true
+        })
+    }
+
+    getStudentById(id) {
+        return connection.select({
+            from: this.tableName,
+            where: {
+                id: id
+            }
+        })
+    }
+
+    removeStudent(id) {
+        return connection.remove({
+            from: this.tableName,
+            where: {
+                id: id
+            }
+        })
+    }
+    searchDrug(word) {
+        return connection.select({
+            from: this.tableName,
+            where: {
+                name: {
+                    like: `%${word}%`
+                },
+            }
+        });
+    }
+
+    updateStudentById(student) {
+        return connection.update({ in: this.tableName,
+            set: {
+                name: student.name,
+                gender: student.gender,
+                country: student.country,
+                city: student.city
+            },
+            where: {
+                id: student.id
+            }
+        })
+    }
+}
