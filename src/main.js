3228132aa6b244832d0c89e1acf093ad3a50a15e
Faisul-Islam <suhid15@gmail.com>
2022-01-13 17:11:49 +0600

    offline icons added

--- src/main.js
+++ src/main.js
@@ -4,33 +4,37 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import  VueHtmlToPaper from 'vue-html-to-paper'
-
+import VueHtmlToPaper from 'vue-html-to-paper'
+import '@mdi/font/css/materialdesignicons.css'
 const options = {
-  name: '_blank',
-  specs: [
-    'fullscreen=yes',
-    'titlebar=yes',
-    'scrollbars=yes'
-  ],
-  styles: [
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-    'https://unpkg.com/kidlat-css/css/kidlat.css',
-  ],
-  timeout: 1000, // default timeout before the print window appears
-  autoClose: true, // if false, the window will not close after printing
-  windowTitle: window.document.title, // override the window title
+    name: '_blank',
+    specs: [
+        'fullscreen=yes',
+        'titlebar=yes',
+        'scrollbars=yes'
+    ],
+    styles: [
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+        'https://unpkg.com/kidlat-css/css/kidlat.css',
+    ],
+    timeout: 1000, // default timeout before the print window appears
+    autoClose: true, // if false, the window will not close after printing
+    windowTitle: window.document.title, // override the window title
 }
 
 Vue.use(VueHtmlToPaper, options);
 
 Vue.use(VueHtmlToPaper)
 
-Vue.config.productionTip = false
-
+// Vue.config.productionTip = false
+// export default new Vuetify({
+//     icons: {
+//         iconfont: 'mdi', // default - only for display purposes
+//     },
+// })
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
